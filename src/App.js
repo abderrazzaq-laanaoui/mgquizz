48242d9dece2fa54179ff14a702dd3f9f6b0abf1
Laanaoui Abderrazzaq <laanaoui.e2305353@etud.univ-ubs.fr>
2024-03-13 15:34:47 +0100

    feat 101 + 201

--- src/App.js
+++ src/App.js
@@ -1,87 +1,42 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import pb from './lib/pocketbase';
-const questions = [
-    {
-      question: "abc",
-      options: [
-        { id: 0, text: "New York City", isCorrect: false },
-        { id: 1, text: "Boston", isCorrect: false },
-        { id: 2, text: "Santa Fe", isCorrect: false },
-        { id: 3, text: "Washington DC", isCorrect: true },
-      ],
-    },
-    {
-      question: "What year was the Constitution of America written?",
-      options: [
-        { id: 0, text: "1787", isCorrect: true },
-        { id: 1, text: "1776", isCorrect: false },
-        { id: 2, text: "1774", isCorrect: false },
-        { id: 3, text: "1826", isCorrect: false },
-      ],
-    },
-    {
-      text: "Who was the second president of the US?",
-      options: [
-        { id: 0, text: "John Adams", isCorrect: true },
-        { id: 1, text: "Paul Revere", isCorrect: false },
-        { id: 2, text: "Thomas Jefferson", isCorrect: false },
-        { id: 3, text: "Benjamin Franklin", isCorrect: false },
-      ],
-    },
-    {
-      text: "What is the largest state in the US?",
-      options: [
-        { id: 0, text: "California", isCorrect: false },
-        { id: 1, text: "Alaska", isCorrect: true },
-        { id: 2, text: "Texas", isCorrect: false },
-        { id: 3, text: "Montana", isCorrect: false },
-      ],
-    },
-    {
-      text: "Which of the following countries DO NOT border the US?",
-      options: [
-        { id: 0, text: "Canada", isCorrect: false },
-        { id: 1, text: "Russia", isCorrect: true },
-        { id: 2, text: "Cuba", isCorrect: true },
-        { id: 3, text: "Mexico", isCorrect: false },
-      ],
-    },
-  ];
+
 
 function App() {
   // Properties
   const [showResults, setShowResults] = useState(false);
-  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showSubmit, setShowSubmit] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(29);
   const [score, setScore] = useState(0);
   const [record, setRecord] = useState(null);
-  const [records, setRecords] = useState(0);
-  const [result, setResult] = useState(0);
-  
-  useEffect(()=>{
-  async function fetchData(){
-  const record = await pb.collection('campagne').getOne('m9o9y1jc2i4nz28');
-  //const record = await pb.collection('campagne').getOne('94pbzhsj2v7xt7b'); //pour les questions de devops
-  console.log("record",record);
-  setRecord(record);
-}
-fetchData()},record)
-  
+  const [answers, setAnswers] = useState([]);
+
+
+  useEffect(() => {
+    async function fetchData() {
+      const record = await pb.collection('campagne').getOne('m9o9y1jc2i4nz28');
+      //const record = await pb.collection('campagne').getOne('94pbzhsj2v7xt7b'); //pour les questions de devops
+      console.log("record", JSON.stringify(record));
+      setRecord(record);
+    }
+    fetchData()
+  }, [])
+
 
   // Helper Functions
 
   /* A possible answer was clicked */
-  const optionClicked = (isCorrect) => {
-    // Increment the score
-    if (isCorrect) {
-      setScore(score + 1);
+  const optionClicked = (questionIndex, answerIndex) => {
+    // check if last question
+    if (currentQuestion === record.questions.length - 1) {
+      setShowSubmit(true);
+    } else {
+      setCurrentQuestion(currentQuestion + 1);
     }
 
-    if (currentQuestion + 1 < record.questions.length) {
-      setCurrentQuestion(currentQuestion + 1);
-    } else {
-      setShowResults(true);
-    }
+    // add the answer to the list of answers
+    setAnswers([...answers, answerIndex]);
   };
 
   /* Resets the game back to default */
@@ -91,63 +46,87 @@ fetchData()},record)
     setShowResults(false);
   };
 
+  const nextQuestion = () => {
+    if (currentQuestion === record.questions.length - 1) {
+      setShowSubmit(true);
+    }
+    setCurrentQuestion(currentQuestion + 1);
+  }
+
+  const previousQuestion = () => {
+    if (currentQuestion === 0) {
+      return;
+    }
+    setCurrentQuestion(currentQuestion - 1);
+  }
+
+  const submitQuizz = () => {
+    // to calculate the score we need to compare the answers with the correct answers
+    // the corre
+  }
+
+
   return (
     <>{record ?
-    <div className="App">
-      {/* 1. Header  */}
-      <h1>{record.nom || ""}</h1>
+      (<div className="App">
+        {/* 1. Header  */}
+        <h1>{record.nom || ""}</h1>
 
-      {/* 2. Current Score  */}
-      <h2>Score: {score}</h2>
 
-      {/* 3. Show results or show the question game  */}
-      {showResults ? (
-        /* 4. Final Results */
-        <div className="final-results">
-          <h1>Final Results</h1>
-          <h2>
-            {score} out of {record.questions.length} correct - (
-            {(score / record.questions.length) * 100}%)
-          </h2>
-          <button onClick={() => restartGame()}>Restart game</button>
-        </div>
-      ) : (
-        /* 5. Question Card  */
-        <div className="question-card">
-          {/* Current Question  */}
-          <h2>
-            Question: {currentQuestion + 1} out of {record.questions.length}
-          </h2>
-          <h3 className="question-text">{record.questions[currentQuestion].question}</h3>
 
-          {/* List of possible answers  */}
-          <ul>
-                <li
-                  onClick={() => optionClicked(currentQuestion, 1)}
-                >
-            {record.questions[currentQuestion].answers[1]}
+        {/* 3. Show results or show the question game  */}
+        {showResults? (<>Your Score is</>) : (showSubmit ? (
+          /* 4. Confirm that you wanna submit page */
+          <div className="final-results" >
+            <h2>Are you sure you want to submit?</h2>
+            <button
+            className="confirm-button"
+              onClick={() => submitQuizz()}
+            >
+              Submit
+            </button>
+            <button 
+            className="cancel-button"
+            onClick={() => setShowSubmit(false)}>Cancel</button>
+          </div>
+        ) : (
+          /* 5. Question Card  */
+          <div className="question-card">
+            {/* Current Question  */}
+            <h2>
+              Question: {currentQuestion + 1} out of {record.questions.length}
+            </h2>
+            <h3 className="question-text">{record.questions[currentQuestion].question}</h3>
+
+            {/* List of possible answers  */}
+            <ul>
+              {Object.values(record.questions[currentQuestion].answers).map((answer, index) => (
+                <li key={index} onClick={() => optionClicked(currentQuestion, index + 1)}>
+                  {answer}
                 </li>
-                <li
-                  onClick={() => optionClicked(currentQuestion, 2)}
-                >
-            {record.questions[currentQuestion].answers[2]}
-                </li>
-                <li
-                  onClick={() => optionClicked(currentQuestion, 3)}
-                >
-            {record.questions[currentQuestion].answers[3]}
-                </li>
-                <li
-                  onClick={() => optionClicked(currentQuestion, 4)}
-                >
-            {record.questions[currentQuestion].answers[4]}
-                </li>
-          </ul>
-        </div>
-      )}
-    </div> : <div>Loading...</div>}
+              ))}
+            </ul>
+            <div className="question-nav">
+              <button className="nav-button" onClick={() => previousQuestion()}  disabled={currentQuestion === 0}>Previous</button>
+          
+              <button className="nav-button" onClick={() => nextQuestion()} disabled={currentQuestion === record.questions.length - 1} >Next</button>
+            </div>
+          </div>
+
+        ))}
+      </div>) : (<div>Loading...</div>)}
     </>
   );
 }
 
 export default App;
+/*
+<div className="final-results">
+            <h1>Final Results</h1>
+            <h2>
+              {score} out of {record.questions.length} correct - (
+              {(score / record.questions.length) * 100}%)
+            </h2>
+            <button className="start-button" onClick={() => restartGame()}>Restart game</button>
+          </div>
+          */
