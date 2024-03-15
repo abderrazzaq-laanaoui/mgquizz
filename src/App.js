474a784bd65593a42487a3378738485f4ff15f25
Ahmed BEN NASRALLAH <bnahmed.2002@gmail.com>
2024-03-15 11:24:02 +0100

    user is being added to pocketbase

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import pb from './lib/pocketbase';
 function App() {
   // Properties
   const [showResults, setShowResults] = useState(false);
-  const [showStart, setShowStart] = useState(true);
   const [showSubmit, setShowSubmit] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
   const [record, setRecord] = useState(null);
   const [answers, setAnswers] = useState([]);
+  const [user, setUser] = useState(null);
+  const [creationMessage, setCreationMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
 
   useEffect(() => {
     async function fetchData() {
       const record = await pb.collection('campagne').getOne('m9o9y1jc2i4nz28');
+      //const record = await pb.collection('campagne').getOne(id);
       //const record = await pb.collection('campagne').getOne('94pbzhsj2v7xt7b'); //pour les questions de devops
       console.log("record", JSON.stringify(record));
       setRecord(record);
@@ -51,12 +54,10 @@ function App() {
   const exit = () => {
     window.location.href = "https://www.mgdis.fr/";
   };
+
   const nextQuestion = () => {
-    console.log("currentQuestion", currentQuestion+1);
-    console.log("record.questions.length", record.questions.length-1);
-    if (currentQuestion === record.questions.length -1 ) {
+    if (currentQuestion === record.questions.length - 1) {
       setShowSubmit(true);
-      return;
     }
     setCurrentQuestion(currentQuestion + 1);
   }
@@ -68,53 +69,72 @@ function App() {
     setCurrentQuestion(currentQuestion - 1);
   }
 
-  const submit = () => {
-    console.log("answers", answers);
-    console.log("rep correctes", record.reponses_correctes);
-    const score = answers.reduce((acc, curr) => {
-      const correctAnswer = record.reponses_correctes.find(item => item.question_id === curr.question_id);
-      if (curr && curr.answer_id) {
-        if (correctAnswer && correctAnswer.correct_answer_id === curr.answer_id) {
-          return acc + 2;
-        } else {
-          return acc - 1;
-        }
+const submit = async()=> {
+  //var name = document.getElementById('name').value;
+  console.log("answers", answers);
+  console.log("rep correctes", record.reponses_correctes);
+  //The name inputted is the user's name
+  const user = document.getElementById("user").value; // Get the value from the input field
+  const score = answers.reduce((acc, curr) => {
+    const correctAnswer = record.reponses_correctes.find(item => item.question_id === curr.question_id);
+    if (curr.answer_id) {
+      if (correctAnswer && correctAnswer.correct_answer_id === curr.answer_id) {
+        return acc + 2;
       } else {
-        return acc;
+        return acc - 1;
       }
-    }, 0);
-    setScore(score);
-    setShowResults(true);
-    setShowSubmit(false);
-    console.log(`User's score is ${score}`);
-  }
-
+    } else {
+      return acc;
+    }
+  }, 0);
+  setScore(score);
+  setUser(user)
+  setShowResults(true);
+  setShowSubmit(false);
+  try {
+    const newCampagne = await pb.collection('passage').create({
+      user: "xp6joalq5r250rp",
+      campagne: "m9o9y1jc2i4nz28",
+      score: score 
+    });
+    setCreationMessage(
+      <>
+        Nouveau user {user} créée <br/>
+        Score ajoutée à la campagne "QuestionCyber"
+      </>
+    );
+  
+          console.log("User et score ajoutées avec succès");
+          setErrorMessage(``);
+        } catch (error) {
+          console.error("Erreur lors de la création du nouveau user", error);
+        }
+      }
+  console.log(`user ${user} score is ${score}`);
 
   return (
     <>{record ?
       (<div className="App">
         {/* 1. Header  */}
         <h1>{record.nom || ""}</h1>
+
+
+
         {/* 3. Show results or show the question game  */}
-        {showResults ? (
-        <div className="final-results" >
-          Merci pour votre participation
-          <h2>Ton Score est {score}</h2>
-          <button className="start-button" onClick={() => exit()}>Exit</button>
-        </div>
-        ) : (showSubmit ? (
+        {showResults? (<> user {user} score is {score}</>) : (showSubmit ? (
           /* 4. Confirm that you wanna submit page */
           <div className="final-results" >
-            <h2>Are you sure you want to submit?</h2>
+            <h2>Add your name</h2>
+            <input type="text" id="user" placeholder="Name"/>
             <button
-              className="confirm-button"
+            className="confirm-button"
               onClick={() => submit()}
             >
               Submit
             </button>
-            <button
-              className="cancel-button"
-              onClick={() => setShowSubmit(false)}>Cancel</button>
+            <button 
+            className="cancel-button"
+            onClick={() => setShowSubmit(false)}>Cancel</button>
           </div>
         ) : (
           /* 5. Question Card  */
@@ -134,9 +154,9 @@ function App() {
               ))}
             </ul>
             <div className="question-nav">
-              <button className="nav-button" onClick={() => previousQuestion()} disabled={currentQuestion === 0}>Previous</button>
-
-              <button className="nav-button" onClick={() => nextQuestion()} disabled={currentQuestion === record.questions.length } >Next</button>
+              <button className="nav-button" onClick={() => previousQuestion()}  disabled={currentQuestion === 0}>Previous</button>
+          
+              <button className="nav-button" onClick={() => nextQuestion()} disabled={currentQuestion === record.questions.length - 1} >Next</button>
             </div>
           </div>
 
@@ -144,6 +164,7 @@ function App() {
       </div>) : (<div>Loading...</div>)}
     </>
   );
+  
 }
 
 export default App;
